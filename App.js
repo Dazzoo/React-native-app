5a76deff59f6b796a73965c4f5e3206bbf50ad03
DazoTT <yuraks46@gmail.com>
2021-08-22 00:55:58 +0300

    First interface

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground  } from 'react-native';
 import Navbar from './src/Navbar'
 import AddToDo from './src/AddToDo'
 
+const image = { uri: "https://reactjs.org/logo-og.png" };
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Navbar/>
-      <AddToDo/>
-    </View>
+          <View style={styles.container}>
+              <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+                  <Navbar/>
+                  <AddToDo/>
+              </ImageBackground>
+          </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { }
+  container: {
+      height: '100%',
+      backgroundColor: '#ffcccb'
+  },
+    image: {
+        flex: 1,
+        justifyContent: "flex-start"
+    }
 });
--- src/AddToDo.js
+++ src/AddToDo.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 const AddToDo = () => {
     return (
         <View style={styles.AddToDoWrapper}>
-            <TextInput style={styles.AddToDoInput}/>
+            <TextInput style={styles.AddToDoInput} />
             <Button style={styles.AddToDoButton} title="Add" />
         </View>
     )
@@ -12,16 +12,21 @@ const AddToDo = () => {
 
 const styles = StyleSheet.create({
     AddToDoWrapper: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+
+        padding: 30
     },
     AddToDoInput: {
+        borderRadius: 3,
         width: '70%',
-        backgroundColor: '#081317',
-        color: 'white',
-        paddingLeft: 10
+        backgroundColor: '#80bcff',
+        color: 'black',
+        paddingLeft: 10,
+        paddingRight: 10
     },
     AddToDoButton: {
-        backgroundColor: 'red'
+    
     }
 })
 
